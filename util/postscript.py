d0cdb6481ada269f75a969f7939c9cf7f417c6fb
Ben <ben@rentenna.com>
2013-09-01 01:27:23 -0400

    #29 added email generation to postscript printer

--- util/postscript.py
+++ util/postscript.py
@@ -13,6 +13,9 @@ class Postscript:
     CODE_FONT = "Courier-New10"
 
     def __init__(self, net_id, module_name, output_home="./output"):
+        self.module_name = module_name
+        self.net_id = net_id
+        self.output_home = output_home
         if not os.path.exists(output_home):
             os.mkdir(output_home)
         module_dir = "%s/%s" % (output_home, module_name)
@@ -53,6 +56,25 @@ class Postscript:
             for line in f:
                 self.enscript_stream.write(line)
 
+    def write_email(self, subject, message):
+        """
+            2013-09-01:
+                Write an email message to `netid`@cornell.edu
+                Store in 'output/email'
+        """
+        email_dir = "%s/email" % self.output_home
+        if not os.path.exists(email_dir):
+            os.mkdir(email_dir)
+        with open("%s/%s-%s.txt" % (email_dir, self.net_id, self.module_name), 'w') as email_message:
+            print>>email_message, message
+        with open("%s/%s-%s.sh" % (email_dir, self.net_id, self.module_name), 'w') as f:
+            print>>f,(" ".join([
+                "mail",
+                "-s",
+                "'%s'" % subject,
+                "'%s@cornell.edu'" % self.net_id,
+            ]))
+
     def write_failures(self, failures):
         self.change_font(self.HEADER_FONT)
         self.enscript_stream.write("\nTest failures:\n")
@@ -65,6 +87,16 @@ class Postscript:
         self.enscript_stream.write("\nNO COMPILE:\n")
         self.change_font(self.CODE_FONT)
         self.enscript_stream.write("%s" % message)
+        # Write an email message
+        email_subject = "[CS3110 test harness] compile error"
+        email_message = "\n".join([
+            "Uh oh! Your submission '%s.ml' did not compile. Compiler output:\n" % self.module_name,
+            "\n".join(message.split("\n")),
+            "\n If this was a trivial error, you have until 11:59PM on Saturday to submit a corrected file to CMS. The course staff will diff your new and old submissions invoke a penalty at their discretion.",
+            "\n---",
+            "Automatically generated message from the CS3110 test harness",
+        ])
+        self.write_email(email_subject, email_message)
 
     def write_success(self):
         self.change_font(self.HEADER_FONT)
